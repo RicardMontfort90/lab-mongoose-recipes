9cd81ee8392cef152a2b486196dc8ed8e3fe48c6
RicardMontfort90 <ricard.montfort.romero@gmail.com>
2022-12-20 16:10:52 +0100

    lab done

--- index.js
+++ index.js
@@ -15,9 +15,44 @@ mongoose
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
+
+  // Iteration 2 - Create a recipe
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    return Recipe.create({
+      title: "allioli with honey",
+      level: "Easy Peasy",
+      ingredients: ["garlic", "oil", "honey", "salt", "white pepper","1 egg"],
+      cuisine: "catalan",
+      dishType: "sauce",
+      duration: 10,
+      creator: R. Montfort
+    })
+    /*return Recipe.create(data)*/
   })
+// Iteration 3 - Insert multiple recipes
+  .then(() =>{
+    return Recipe.insertMany(data)
+  })
+// Iteration 4 - Update recipe
+  .then(() => {
+    return Recipe.findOneAndUpdate({ 
+      title: "Rigatoni alla Genovese" },
+      { 
+        duration: 100
+    });
+  })
+// Iteration 5 - Remove a recipe
+  .then(() => {
+    return Recipe.deleteOne({
+      title: "Carrot Cake"
+    });
+  })
+// Iteration 6 - Close the Database
+  .then(() => {
+    mongoose.connection.close();
+  })
+
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
